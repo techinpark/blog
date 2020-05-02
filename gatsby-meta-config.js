28e2d19f46ef27c4f3d88c4a46ce8986511b058b
Fernando <fernando@kakao.com>
2020-05-02 15:37:19 +0900

    fix(config): change meta config

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -2,8 +2,8 @@ module.exports = {
   title: `Fernando 기술 블로그`,
   description: `생각하고 배운것을 정리하기 위한 블로그입니다`,
   author: `fernando`,
-  introduction: `새로운 오픈소스를 적용하고 만드는것을 좋아합니다.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  introduction: `iOS 개발자이며 다양한 업무 자동화에 관심이 많습니다.`,
+  siteUrl: `https://fernando.kr`, // Your blog site url
   social: {
     twitter: `techinpark`, // Your Twitter account
     github: `techinpark`, // Your GitHub account
@@ -15,10 +15,10 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'techinpark/blog-comments', // Your repository for archive comment
   },
   configs: {
-    countOfInitialPost: 10, // Config your initial count of post
+    countOfInitialPost: 20, // Config your initial count of post
   },
   sponsor: {
     buyMeACoffeeId: 'techinpark',
@@ -26,5 +26,5 @@ module.exports = {
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: '', // Add your google analytics tranking ID
+  ga: 'UA-23683901-16', // Add your google analytics tranking ID
 }
